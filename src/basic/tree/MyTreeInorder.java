1f86baa4ee47ade3e18c37bc65fb3081d713bb3c
zhulx <2428784410@qq.com>
2025-09-11 09:13:43 +0800

    94. 二叉树的中序遍历

---
+++ src/basic/tree/MyTreeInorder.java
@@ -0,0 +1,45 @@
+package basic.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zhulx
+ **/
+public class MyTreeInorder {
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        inorder(root, res);
+        return res;
+    }
+
+    public void inorder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
+    }
+}
