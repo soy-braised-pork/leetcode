6e72e0d04fa24b8534d9983b46b996e61f249d1d
zhulx <2428784410@qq.com>
2025-09-12 14:35:08 +0800

    114. 二叉树展开为链表

---
+++ src/basic/tree/MyTreeToList.java
@@ -0,0 +1,42 @@
+package basic.tree;
+
+/**
+ * @author zhulx
+ **/
+public class MyTreeToList {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    public void flatten(TreeNode root) {
+        while (root != null) {
+            //左子树为 null，直接考虑下一个节点
+            if (root.left == null) {
+                root = root.right;
+            } else {
+                // 找左子树最右边的节点
+                TreeNode pre = root.left;
+                while (pre.right != null) {
+                    pre = pre.right;
+                }
+                //将原来的右子树接到左子树的最右边节点
+                pre.right = root.right;
+                // 将左子树插入到右子树的地方
+                root.right = root.left;
+                root.left = null;
+                // 考虑下一个节点
+                root = root.right;
+            }
+        }
+    }
+}
