55ea9d0a4a5a965695897d23d30186326a5c38a7
zhulx <2428784410@qq.com>
2025-09-16 09:10:02 +0800

    141. 环形链表

---
+++ src/basic/linked/LinkedListCycle.java
@@ -0,0 +1,31 @@
+package basic.linked;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author zhulx
+ **/
+public class LinkedListCycle {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> seen = new HashSet<ListNode>();
+        while (head != null) {
+            if (!seen.add(head)) {
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+
+}
