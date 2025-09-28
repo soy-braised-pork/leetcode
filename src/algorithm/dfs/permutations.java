f606750e3fca1c7cf80bdfc2070c10bc96bad9b7
zhulx <2428784410@qq.com>
2025-09-28 16:24:46 +0800

    46. 全排列

---
+++ src/algorithm/dfs/permutations.java
@@ -0,0 +1,54 @@
+package algorithm.dfs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zhulx
+ **/
+public class permutations {
+
+
+    public List<List<Integer>> permute(int[] nums) {
+        int len = nums.length;
+        // 使用一个动态数组保存所有可能的全排列
+        List<List<Integer>> res = new ArrayList<>();
+        if (len == 0) {
+            return res;
+        }
+
+        boolean[] used = new boolean[len];
+        List<Integer> path = new ArrayList<>();
+
+        dfs(nums, len, 0, path, used, res);
+        return res;
+    }
+
+    private void dfs(int[] nums, int len, int depth,
+                     List<Integer> path, boolean[] used,
+                     List<List<Integer>> res) {
+        if (depth == len) {
+            res.add(path);
+            return;
+        }
+
+        for (int i = 0; i < len; i++) {
+            if (!used[i]) {
+                path.add(nums[i]);
+                used[i] = true;
+
+                dfs(nums, len, depth + 1, path, used, res);
+                used[i] = false;
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        permutations per = new permutations();
+        List<List<Integer>> lists = per.permute(nums);
+        System.out.println(lists);
+    }
+
+}
